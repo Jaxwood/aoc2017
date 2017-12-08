551f6a7fd4ec3f61cf021985496175f87a1e45b9
Jacob Lorenzen <lorenzen.jacob@gmail.com>
2017-12-08 20:53:27 +0100

    setup for day5

---
+++ src/Advent/Day5.hs
@@ -0,0 +1,9 @@
+module Advent.Day5 (day5a, day5b) where
+
+  import Data.List
+
+  day5a :: String -> Int
+  day5a a = 0
+
+  day5b :: String -> Int
+  day5b a = 0
---
+++ test/Advent/Day5Spec.hs
@@ -0,0 +1,17 @@
+module Advent.Day5Spec (spec) where
+
+  import Test.Hspec
+  import Advent.Day5 (day5a, day5b)
+  import System.Directory
+
+  main :: IO ()
+  main = do
+    hspec spec
+
+  spec :: Spec
+  spec = do
+    describe "day5a" $ do
+      it "reach exit" $ do
+        dir <- getCurrentDirectory
+        csv <- readFile (dir ++ "/test/day5.csv")
+        day5a csv  `shouldBe` 5
