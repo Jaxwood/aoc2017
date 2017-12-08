b09ad54c13443c7c679148d198b78c3b11088575
Jacob Lorenzen <lorenzen.jacob@gmail.com>
2017-12-08 20:12:23 +0100

    solution for day4b

--- src/Advent/Day4.hs
+++ src/Advent/Day4.hs
@@ -5,6 +5,7 @@ module Advent.Day4 (day4a, day4b) where
   day4a :: String -> Int
   day4a = length . filter (\x -> x == False) . map (any (>1) . map length . group . sort . words) . lines
 
-  day4b :: Int -> Int
-  day4b = id
+  day4b :: String -> Int
+  day4b = length . filter (\x -> x == False) . map (any (>1) . map length . group . map sort . sort . words) . lines
+
 
--- test/Advent/Day4Spec.hs
+++ test/Advent/Day4Spec.hs
@@ -17,3 +17,14 @@ module Advent.Day4Spec (spec) where
         day4a "aa bb cc dd ee aa" `shouldBe` 0
       it "duplicate but different length" $ do
         day4a "aa bb cc dd ee aaa" `shouldBe` 1
+    describe "day4b" $ do
+      it "no duplicate" $ do
+        day4b "abcde fghij" `shouldBe` 1
+      it "duplicate anagram" $ do
+        day4b "abcde xyz ecdab" `shouldBe` 0
+      it "all letter should be used for anagram" $ do
+        day4b "a ab abc abd abf abj" `shouldBe` 1
+      it "valid word" $ do
+        day4b "iiii oiii ooii oooi oooo" `shouldBe` 1
+      it "invalid word" $ do
+        day4b "oiii ioii iioi iiio" `shouldBe` 0
