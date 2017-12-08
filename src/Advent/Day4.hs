857df0b648930151330e31120fdb4ba413c01bc3
Jacob Lorenzen <lorenzen.jacob@gmail.com>
2017-12-08 19:32:12 +0100

    solution for day4a

--- src/Advent/Day4.hs
+++ src/Advent/Day4.hs
@@ -3,7 +3,7 @@ module Advent.Day4 (day4a, day4b) where
   import Data.List
 
   day4a :: String -> Bool
-  day4a a = False
+  day4a = all (==1) . map length . group . sort . words
 
   day4b :: Int -> Int
   day4b = id
