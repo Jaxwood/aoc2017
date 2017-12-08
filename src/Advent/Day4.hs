f88cdf95336be8c56d9cd5658ef3f2da8432e4a4
Jacob Lorenzen <lorenzen.jacob@gmail.com>
2017-12-08 20:49:19 +0100

    sort after word sorting

--- src/Advent/Day4.hs
+++ src/Advent/Day4.hs
@@ -6,6 +6,6 @@ module Advent.Day4 (day4a, day4b) where
   day4a = length . filter (\x -> x == False) . map (any (>1) . map length . group . sort . words) . lines
 
   day4b :: String -> Int
-  day4b = length . filter (\x -> x == False) . map (any (>1) . map length . group . map sort . sort . words) . lines
+  day4b = length . filter (\x -> x == False) . map (any (>1) . map length . group . sort . map sort . words) . lines
 
 
