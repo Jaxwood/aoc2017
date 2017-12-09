7c0d1b36263e3b219df656c5f51730e05de8342c
Jaxwood <lorenzen.jacob@gmail.com>
2017-12-09 16:49:19 +0100

    refactor solution for day3

--- src/Advent/Day3.hs
+++ src/Advent/Day3.hs
@@ -3,54 +3,28 @@ module Advent.Day3 (day3a, day3b) where
   import Data.List
 
   day3a :: Int -> Int
-  day3a = sumPairs . toPairs
+  day3a 1 = 0
+  day3a n = getValue n $ generate $ head $ filter (\(l,h,_) -> n > l && n < h) $ map bounds [1..]
 
   day3b :: Int -> Int
   day3b = id
 
-  toPairs :: Int -> (Int, (Int, Int))
-  toPairs 1 = (1, (0,0))
-  toPairs a = let c@(l,h) = range a
-              in head $ filter (\(f,s) -> f == a) $ zip [l..h] $ coords c (rangeIdx c)
+  getValue :: Int -> [(Int, (Int, Int))] -> Int
+  getValue n xs = let (_, (f,l)) = head $ filter (\(x, _) -> x == n) xs
+                  in abs f + abs l
 
-  sumPairs :: (Int, (Int,Int)) -> Int
-  sumPairs (_,(a,b)) = abs a + abs b
+  generate :: (Int,Int,Int) -> [(Int, (Int,Int))]
+  generate (f,l,n) = let right = [(n,x) |x <- [-n+1..n]]
+                         top = [(x,n) |x <- reverse [-n..n-1]]
+                         left = [(-n,x) |x <- reverse [-n..n-1]]
+                         bottom = [(x,-n) |x <- [-n+1..n]]
+                     in zip [f..l] $ right ++ top ++ left ++ bottom
 
-  range :: Int -> (Int,Int)
-  range a = head $ filter (\(l,h) -> l < a && a < h) ranges
+  interval :: [Int]
+  interval = [3,5..]
 
-  ranges :: [(Int, Int)]
-  ranges = map (\x -> ((x-2) * (x-2) + 1,x * x)) [3,5..]
-
-  rangeIdx :: (Int,Int) -> Int
-  rangeIdx n = case findIndex ((==)n) ranges of
-               Just x -> succ x
-               Nothing -> 0
-
-  coords :: (Int,Int) -> Int -> [(Int,Int)]
-  coords (l,h) n = foldl (\acc (ls,f) -> (init acc) ++ foldl (\acc' _ -> acc' ++ [f $ last acc']) [last acc] ls) [first] $ zip [[l..tr'], [tr..tl'], [tl..bl'], [bl..br]] ops 
-                   where n' = n+n
-                         n'' = -n
-                         first = (n,succ n'')
-                         br = pred h
-                         bl = h-n'
-                         bl' = pred bl
-                         tl = bl - n'
-                         tl' = pred tl
-                         tr = tl - n'
-                         tr' = pred tr
-
-  ops :: [(Int,Int) -> (Int,Int)]
-  ops = [addY, decX, decY, addX]
-
-  addX :: (Int,Int) -> (Int,Int)
-  addX (a,b) = (succ a,b)
-
-  decX :: (Int,Int) -> (Int,Int)
-  decX (a,b) = (pred a,b)
-
-  addY :: (Int,Int) -> (Int,Int)
-  addY (a,b) = (a,succ b)
-
-  decY :: (Int,Int) -> (Int,Int)
-  decY (a,b) = (a,pred b)
+  bounds :: Int -> (Int,Int,Int)
+  bounds 1 = (2,9,1)
+  bounds n = let high = interval !! (n - 1)
+                 low = interval !! (n - 2)
+             in (low * low + 1, high * high, n)
