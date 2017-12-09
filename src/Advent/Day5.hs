7a8f3664ac723f08363f2608c43495f3b91b6f8d
Jacob Lorenzen <lorenzen.jacob@gmail.com>
2017-12-09 10:32:48 +0100

    use sequence for day5b

--- src/Advent/Day5.hs
+++ src/Advent/Day5.hs
@@ -7,7 +7,7 @@ module Advent.Day5 (day5a, day5b) where
   day5a = jumps 0 0 . fromList . map read . lines
 
   day5b :: String -> Int
-  day5b a = 0
+  day5b = jumps' 0 0 . fromList . map read . lines
 
   jumps :: Int -> Int -> Seq Int -> Int
   jumps idx acc xs = if S.length xs <= idx
@@ -18,3 +18,13 @@ module Advent.Day5 (day5a, day5b) where
                            idx' = if x == 0 then idx else idx + x
                            (f,(S.viewl -> l:<ls)) = S.splitAt idx xs
                        in jumps idx' (succ acc) $ f >< (succ l) <| ls
+
+  jumps' :: Int -> Int -> Seq Int -> Int
+  jumps' idx acc xs =  if S.length xs <= idx
+                       then 
+                         acc
+                       else
+                         let x = index xs idx
+                             idx' = if x == 0 then idx else idx + x
+                             (f,(S.viewl -> l:<ls)) = S.splitAt idx xs
+                         in jumps' idx' (succ acc) $ f >< (if l > 2 then pred l else succ l) <| ls
