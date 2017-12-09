7e125eabd83e78c7987955cd1eaacf9cc0437245
Jacob Lorenzen <lorenzen.jacob@gmail.com>
2017-12-09 10:30:04 +0100

    use sequence for performance

--- src/Advent/Day5.hs
+++ src/Advent/Day5.hs
@@ -1,29 +1,20 @@
+{-# LANGUAGE ViewPatterns #-}
 module Advent.Day5 (day5a, day5b) where
 
-  import Data.List
+  import Data.Sequence as S
 
   day5a :: String -> Int
-  day5a = jumps 0 0 . map read . lines
+  day5a = jumps 0 0 . fromList . map read . lines
 
   day5b :: String -> Int
-  day5b = jumps' 0 0 . map read . lines
+  day5b a = 0
 
-  jumps :: Int -> Int -> [Int] -> Int
-  jumps idx acc xs = if length xs <= idx
+  jumps :: Int -> Int -> Seq Int -> Int
+  jumps idx acc xs = if S.length xs <= idx
                      then 
                        acc
                      else
-                       let x = xs !! idx
+                       let x = index xs idx
                            idx' = if x == 0 then idx else idx + x
-                           (f,(l:ls)) = splitAt idx xs
-                       in jumps idx' (succ acc) $ f ++ (succ l):ls
-
-  jumps' :: Int -> Int -> [Int] -> Int
-  jumps' idx acc xs = if length xs <= idx
-                      then 
-                        acc
-                      else
-                        let x = xs !! idx
-                            idx' = if x == 0 then idx else idx + x
-                            (f,(l:ls)) = splitAt idx xs
-                        in jumps' idx' (succ acc) $ f ++ (if x > 2 then pred l else succ l):ls
+                           (f,(S.viewl -> l:<ls)) = S.splitAt idx xs
+                       in jumps idx' (succ acc) $ f >< (succ l) <| ls
