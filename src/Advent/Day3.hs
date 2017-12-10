d37e23b483792e99b7750a96e5d1e9933bace224
Jacob Lorenzen <lorenzen.jacob@gmail.com>
2017-12-10 12:33:06 +0100

    solution for day3b - sort of a hack

--- src/Advent/Day3.hs
+++ src/Advent/Day3.hs
@@ -4,14 +4,36 @@ module Advent.Day3 (day3a, day3b) where
 
   day3a :: Int -> Int
   day3a 1 = 0
-  day3a n = getValue n $ generate $ head $ filter (\(l,h,_) -> n > l && n < h) $ map bounds [1..]
+  day3a n = toValue $ head $ filter (port n) $ concatMap generate $ filter (inBounds n) $ map bounds [1..]
 
   day3b :: Int -> Int
-  day3b = id
+  day3b n = toValue' $ head . dropWhile (\(x,_) -> x <= n) $ foldl findNeighbors [] coords
 
-  getValue :: Int -> [(Int, (Int, Int))] -> Int
-  getValue n xs = let (_, (f,l)) = head $ filter (\(x, _) -> x == n) xs
-                  in abs f + abs l
+  toValue :: (Int,(Int,Int)) -> Int
+  toValue (_, (l,h)) = abs l + abs h
+
+  toValue' :: (Int,(Int,Int)) -> Int
+  toValue'(n, _) = n
+
+  inBounds :: Int -> (Int, Int, Int) -> Bool
+  inBounds n (l,h,_) = n > l && n < h
+
+  port :: Int -> (Int, (Int, Int)) -> Bool
+  port n (x,_) = x == n
+
+  coords :: [(Int,(Int,Int))]
+  coords = (1, (0,0)):concatMap (generate . bounds) [1..50]
+
+  findNeighbors :: [(Int, (Int,Int))] -> (Int,(Int,Int)) -> [(Int, (Int,Int))]
+  findNeighbors acc h@(1,(0,0)) = acc ++ [h]
+  findNeighbors acc (_,c) = let n' = sumCells c acc
+                            in acc ++ [(n',c)]
+
+  sumCells :: (Int,Int) -> [(Int, (Int,Int))] -> Int
+  sumCells c cs = foldr (\(n,_) acc -> acc + n) 0 $ filter (\(n, c') -> c' `elem` (cells c)) cs
+
+  cells :: (Int,Int) -> [(Int,Int)]
+  cells (x,y) = [(x',y')|x' <- [pred x..succ x], y' <- [pred y..succ y], (x,y) /= (x',y')]
 
   generate :: (Int,Int,Int) -> [(Int, (Int,Int))]
   generate (f,l,n) = let right = [(n,x) |x <- [-n+1..n]]
--- test/Advent/Day3Spec.hs
+++ test/Advent/Day3Spec.hs
@@ -13,9 +13,12 @@ module Advent.Day3Spec (spec) where
     describe "day3a" $ do
       it "1 is 0 steps away" $ do
         day3a 1 `shouldBe` 0
-      it "12 is 0 steps away" $ do
+      it "12 is 3 steps away" $ do
         day3a 12 `shouldBe` 3
-      it "1 is 0 steps away" $ do
+      it "1 is 2 steps away" $ do
         day3a 23 `shouldBe` 2
-      it "1 is 0 steps away" $ do
+      it "1 is 31 steps away" $ do
         day3a 1024 `shouldBe` 31
+    describe "day3b" $ do
+      it "after 10 comes 11" $ do
+        day3b 10 `shouldBe` 11
