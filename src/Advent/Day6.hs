f2c2f57ff10f5199946204ddd191c169effb7a43
Jacob Lorenzen <lorenzen.jacob@gmail.com>
2017-12-13 09:37:38 +0100

    solution for day6a

--- src/Advent/Day6.hs
+++ src/Advent/Day6.hs
@@ -1,7 +1,26 @@
 module Advent.Day6 (day6a, day6b) where
 
+  import Data.List
+
   day6a :: String -> Int
-  day6a a = sum $ map read $ words a
+  day6a a = findMax [] $ map read $ words a
 
   day6b :: String -> Int
   day6b a = 0
+
+  findMax :: [[Int]] -> [Int] -> Int
+  findMax acc xs
+    | xs `elem` acc = length acc
+    | otherwise = let m = maximum xs
+                      idx = findIndex ((==)m) xs
+                      candidate = redistribute xs m idx
+                  in findMax (xs:acc) candidate
+
+
+  redistribute :: [Int] -> Int -> Maybe Int -> [Int]
+  redistribute xs m (Nothing) = xs
+  redistribute xs m (Just idx) =
+    let ln = pred $ length xs
+        am = div m ln
+        r = m `mod` ln
+    in map (\(i,v) -> if i == idx then r else v + am) $  zip [0..] xs
