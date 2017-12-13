d389087f74611b796d511915862b711da0714190
Jacob Lorenzen <lorenzen.jacob@gmail.com>
2017-12-13 11:36:20 +0100

    handle case where max number is less than length of list

--- src/Advent/Day6.hs
+++ src/Advent/Day6.hs
@@ -16,11 +16,20 @@ module Advent.Day6 (day6a, day6b) where
                       candidate = redistribute xs m idx
                   in findMax (xs:acc) candidate
 
-
   redistribute :: [Int] -> Int -> Maybe Int -> [Int]
-  redistribute xs m (Nothing) = xs
-  redistribute xs m (Just idx) =
-    let ln = pred $ length xs
-        am = div m ln
-        r = m `mod` ln
-    in map (\(i,v) -> if i == idx then r else v + am) $  zip [0..] xs
+  redistribute xs _ (Nothing) = xs
+  redistribute xs mx (Just idx)
+    | mx < ln = let a = take ln $ 0:(distribute xs' mx)
+                in take ln $ drop (ln - idx) $ cycle a
+    | otherwise = map (\(idx',val) -> if idx' == idx then remainder else val+quotient) $ zip [0..] xs
+    where ln = length xs
+          ln' = pred ln
+          remainder = mx `mod` ln'
+          quotient = mx `div` ln'
+          xs' = drop (succ idx) $ cycle xs
+
+  distribute :: [Int] -> Int -> [Int]
+  distribute xs 0 = xs
+  distribute [] _ = []
+  distribute (x:xs) v = (succ x):(distribute xs $ pred v)
+    
