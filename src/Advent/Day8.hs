62cf606245d55d3496779a9110ee46698d580a2c
Jacob Lorenzen <lorenzen.jacob@gmail.com>
2017-12-23 19:11:01 +0100

    setup for day8

---
+++ src/Advent/Day8.hs
@@ -0,0 +1,12 @@
+module Advent.Day8 (day8a, day8b) where
+
+  import Data.List
+  import Data.Map
+  import Text.Parsec
+  import Text.Parsec.String
+
+  day8a :: String -> String
+  day8a = id
+
+  day8b :: String -> Int
+  day8b s = 0
---
+++ test/Advent/Day8Spec.hs
@@ -0,0 +1,22 @@
+module Advent.Day8Spec (spec) where
+
+  import Test.Hspec
+  import Advent.Day8 (day8a, day8b)
+  import System.Directory
+
+  main :: IO ()
+  main = do
+    hspec spec
+
+  spec :: Spec
+  spec = do
+    describe "day8a" $ do
+      it "reach exit" $ do
+        dir <- getCurrentDirectory
+        csv <- readFile (dir ++ "/test/day8.csv")
+        day8a csv  `shouldBe` ""
+    describe "day8b" $ do
+      it "reach exit" $ do
+        dir <- getCurrentDirectory
+        csv <- readFile (dir ++ "/test/day8.csv")
+        day8b csv  `shouldBe` 0
