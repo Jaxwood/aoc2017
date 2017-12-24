95cccf365cb6540f84e909fad736879eff7cb758
Jaxwood <lorenzen.jacob@gmail.com>
2017-12-24 14:43:56 +0100

    setup for day9

---
+++ src/Advent/Day9.hs
@@ -0,0 +1,10 @@
+module Advent.Day9 (day9a, day9b) where
+
+  import Text.Parsec
+  import Text.Parsec.String
+
+  day9a :: String -> Int
+  day9a s = 0
+
+  day9b :: String -> Int
+  day9b s = 0
---
+++ test/Advent/Day9Spec.hs
@@ -0,0 +1,35 @@
+module Advent.Day9Spec (spec) where
+
+  import Test.Hspec
+  import Advent.Day9 (day9a, day9b)
+  import Data.Map
+  import System.Directory
+
+  main :: IO ()
+  main = do
+    hspec spec
+
+  spec :: Spec
+  spec = do
+    describe "day9a" $ do
+      it "{} scores 1" $ do
+        day9a "{}"  `shouldBe` 1
+      it "{{{}}} scores 6" $ do
+        day9a "{{{}}}"  `shouldBe` 6
+      it "{{},{}} scores 5" $ do
+        day9a "{{},{}}"  `shouldBe` 5
+      it "{{{},{},{{}}}} scores 16" $ do
+        day9a "{{{},{},{{}}}}"  `shouldBe` 16
+      it "{<a>,<a>,<a>,<a>} scores 1" $ do
+        day9a "{<a>,<a>,<a>,<a>}"  `shouldBe` 1
+      it "{{<ab>},{<ab>},{<ab>},{<ab>}} scores 9" $ do
+        day9a "{{<ab>},{<ab>},{<ab>},{<ab>}}"  `shouldBe` 9
+      it "{{<!!>},{<!!>},{<!!>},{<!!>}} scores 9" $ do
+        day9a "{{<!!>},{<!!>},{<!!>},{<!!>}}"  `shouldBe` 9
+      it "{{<a!>},{<a!>},{<a!>},{<ab>}} scores 3" $ do
+        day9a "{{<a!>},{<a!>},{<a!>},{<ab>}}"  `shouldBe` 3
+    describe "day9b" $ do
+      it "reach exit" $ do
+        dir <- getCurrentDirectory
+        csv <- readFile (dir ++ "/test/day9.csv")
+        day9b csv  `shouldBe` 0
