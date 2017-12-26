8762c89915529f79d527a78afe2c8af2a97058ae
Jacob Lorenzen <lorenzen.jacob@gmail.com>
2017-12-26 13:31:22 +0100

    solution for day10a

--- src/Advent/Day10.hs
+++ src/Advent/Day10.hs
@@ -1,9 +1,11 @@
 module Advent.Day10 (day10a, day10b) where
 
   import Data.List
+  import qualified Data.Text as T
 
-  day10a :: [Int] -> [Int] -> Int
-  day10a xs ys = check $ hash 0 xs ys
+  day10a :: [Int] -> String -> Int
+  day10a xs ys = let ys' = map (read . T.unpack) $ T.split (==',') $ T.pack ys
+                 in check $ hash (0,0) xs ys'
 
   day10b :: String -> Int
   day10b s = 0
@@ -11,14 +13,28 @@ module Advent.Day10 (day10a, day10b) where
   check :: [Int] -> Int
   check (x:y:xs) = x * y
 
-  hash :: Int -> [Int] -> [Int] -> [Int]
+  hash :: (Int,Int) -> [Int] -> [Int] -> [Int]
   hash s xs [] = xs
-  hash s xs (y:ys) = let xs' = rmi s y xs
-                     in hash (succ s) xs' ys
+  hash (s,idx) xs (y:ys) = let ys' = reverse' idx y xs
+                               xl = length xs
+                               idx' = position xl y idx s
+                               xs' = stitch idx xs ys' 
+                           in hash (succ s, idx') xs' ys
 
-  rmi :: Int -> Int -> [Int] -> [Int]
-  rmi s y xs = let xs' = cycle xs
-                   xs'' = reverse $ take y $ drop s xs' 
-                   xs''' = drop y xs'
-               in xs
+  stitch :: Int -> [Int] -> [Int] -> [Int]
+  stitch idx xs ys = rotate (xl-idx) $ take xl $ ys ++ drop yl ys'
+                     where xl = length xs
+                           yl = length ys
+                           ys' = rotate idx $ cycle xs
 
+  reverse' :: Int -> Int -> [Int] -> [Int]
+  reverse' idx y xs = reverse $ take y $ drop idx $ cycle xs 
+
+  position :: Int -> Int -> Int -> Int -> Int
+  position len amnt idx skp = if a <= 0 then (idx + amnt + skp) - len else b
+                              where a = len - b
+                                    b = idx + amnt + skp
+                                    
+  rotate :: Int -> [a] -> [a]
+  rotate _ [] = []
+  rotate n xs = zipWith const (drop n (cycle xs)) xs
--- test/Advent/Day10Spec.hs
+++ test/Advent/Day10Spec.hs
@@ -2,7 +2,6 @@ module Advent.Day10Spec (spec) where
 
   import Test.Hspec
   import Advent.Day10 (day10a, day10b)
-  import Data.Map
   import System.Directory
 
   main :: IO ()
@@ -12,8 +11,10 @@ module Advent.Day10Spec (spec) where
   spec :: Spec
   spec = do
     describe "day10a" $ do
-      it "{} scores 12" $ do
-        day10a [0..4] [3,4,1,5]  `shouldBe` 12
-    describe "day10b" $ do
-      it "{} scores 1" $ do
-        day10b "{}"  `shouldBe` 0
+      it "scores 12" $ do
+        dir <- getCurrentDirectory
+        csv <- readFile (dir ++ "/test/day10.csv")
+        day10a [0..4] csv `shouldBe` 12
+      describe "day10b" $ do
+        it "day10b" $ do
+          day10b ""  `shouldBe` 0
