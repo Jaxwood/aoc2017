5029f6169c0a97db21250703db2f29f1ad2e9e85
Jacob Lorenzen <lorenzen.jacob@gmail.com>
2017-12-28 14:45:20 +0100

    setup for day13

---
+++ src/Advent/Day13.hs
@@ -0,0 +1,7 @@
+module Advent.Day13 (day13a, day13b) where
+
+  day13a :: String -> Int
+  day13a s = 0
+
+  day13b :: String -> Int
+  day13b s = 0
---
+++ test/Advent/Day13Spec.hs
@@ -0,0 +1,22 @@
+module Advent.Day13Spec (spec) where
+
+  import Test.Hspec
+  import Advent.Day13 (day13a, day13b)
+  import System.Directory
+
+  main :: IO ()
+  main = do
+    hspec spec
+
+  spec :: Spec
+  spec = do
+    describe "day13a" $ do
+      it "should find severity" $ do
+        dir <- getCurrentDirectory
+        csv <- readFile (dir ++ "/test/day13.csv")
+        day13a csv `shouldBe` 24
+    describe "day13b" $ do
+      it "should find severity" $ do
+        dir <- getCurrentDirectory
+        csv <- readFile (dir ++ "/test/day13.csv")
+        day13b csv `shouldBe` 0
