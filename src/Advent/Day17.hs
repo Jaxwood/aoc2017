ef72fff6a8b1df06670cc45d8a1ee18989cc400e
Jacob Lorenzen <lorenzen.jacob@gmail.com>
2018-01-03 22:52:53 +0100

    use sequence and solve day17b

--- src/Advent/Day17.hs
+++ src/Advent/Day17.hs
@@ -1,19 +1,22 @@
 module Advent.Day17 (day17a, day17b) where
 
-    import Data.List
+    import qualified Data.Sequence as Seq
 
     day17a :: Int -> Int
-    day17a s = spinlock 1 s 0 [0]
+    day17a s = spinlock 1 s 0 (Seq.singleton 0)
 
     day17b :: Int -> Int
-    day17b i = i
+    day17b s = spinlock' 1 s 0 (Seq.singleton 0)
 
-    spinlock :: Int -> Int -> Int -> [Int] -> Int
-    spinlock 2018 _ _ is = let (Just a) = elemIndex 2017 is
-                               (hd,ls) = splitAt (succ a) (cycle is)
-                           in head ls
-    spinlock num size idx is = let a = (drop idx $ cycle is) !! size
-                                   (Just b) = elemIndex a is
-                                   idx' = succ b
-                                   (hd,ls) = splitAt idx' is
-                               in spinlock (succ num) size idx' (hd ++ (num:ls))
+    spinlock :: Int -> Int -> Int -> Seq.Seq Int -> Int
+    spinlock 2018 _ _ is = let (Just idx) = Seq.elemIndexL 2017 is
+                           in Seq.index is (succ idx)
+    spinlock num size idx is = let idx' = (idx + size) `mod` (Seq.length is)
+                                   idx'' = succ idx'
+                               in spinlock (succ num) size idx'' (Seq.insertAt idx'' num is)
+
+    spinlock' :: Int -> Int -> Int -> Seq.Seq Int -> Int
+    spinlock' 50000001 _ _ is = Seq.index is 1
+    spinlock' num size idx is = let idx' = (idx + size) `mod` (Seq.length is)
+                                    idx'' = succ idx'
+                                in spinlock' (succ num) size idx'' (Seq.insertAt idx'' num is)
--- test/Advent/Day17Spec.hs
+++ test/Advent/Day17Spec.hs
@@ -12,3 +12,7 @@ module Advent.Day17Spec (spec) where
     describe "day17a" $ do
       it "should find the value after 2017" $ do
         day17a 3 `shouldBe` 638
+    describe "day17b" $ do
+      it "should find the value after 50000000" $ do
+        pendingWith "slow test"
+        day17b 3 `shouldBe` 1222153
