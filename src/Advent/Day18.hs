687e412d656de0c4d2906755d909bb6f9c178c76
Jacob Lorenzen <lorenzen.jacob@gmail.com>
2018-01-06 08:47:23 +0100

    refactor code to contains less datatypes

--- src/Advent/Day18.hs
+++ src/Advent/Day18.hs
@@ -1,24 +1,21 @@
 module Advent.Day18 (day18a, day18b) where
 
   import qualified Data.Map.Strict as M
+  import Data.Char
   import Data.List
   import Data.Either (rights)
   import Text.Parsec
   import Text.Parsec.String
 
+  data Value = Number Int | Register Char deriving (Show,Eq)
   data Instruction =
-      Set Char Int
-    | SetChar Char Char
-    | Add Char Int
-    | AddChar Char Char
-    | Mul Char Int
-    | MulChar Char Char
-    | Mod Char Int
-    | ModChar Char Char
+      Set Char Value
+    | Add Char Value
+    | Mul Char Value
+    | Mod Char Value
     | Snd Char
     | Rcv Char
-    | JgzChar Char Char
-    | Jgz Char Int deriving (Show,Eq)
+    | Jgz Char Value deriving (Show,Eq)
 
   day18a :: String -> Int
   day18a s = let is = rights $ map parseInput $ lines s
@@ -29,18 +26,13 @@ module Advent.Day18 (day18a, day18b) where
 
   runInstruction :: M.Map Char Int -> [Instruction] -> [Instruction] -> Int
   runInstruction m s [] = 0
-  runInstruction m s (s'@(Set c i):is) = runInstruction (M.insert c i m) s is
-  runInstruction m s (s'@(SetChar c c'):is) = runInstruction (M.insert c (lookup' m c') m) s is
-  runInstruction m s (s'@(Add c i):is) = runInstruction (update' m (+) c i) s is
-  runInstruction m s (s'@(AddChar c c'):is) = runInstruction (update' m (+) c (lookup' m c')) s is
-  runInstruction m s (s'@(Mul c i):is) = runInstruction (update' m (*) c i) s is
-  runInstruction m s (s'@(MulChar c c'):is) = runInstruction (update' m (*) c (lookup' m c')) s is
-  runInstruction m s (s'@(Mod c i):is) = runInstruction (update' m mod c i) s is
-  runInstruction m s (s'@(ModChar c c'):is) = runInstruction (update' m mod c (lookup' m c')) s is
+  runInstruction m s (s'@(Set c i):is) = runInstruction (M.insert c (lookup'' m i) m) s is
+  runInstruction m s (s'@(Add c i):is) = runInstruction (update' m (+) c (lookup'' m i)) s is
+  runInstruction m s (s'@(Mul c i):is) = runInstruction (update' m (*) c (lookup'' m i)) s is
+  runInstruction m s (s'@(Mod c i):is) = runInstruction (update' m mod c (lookup'' m i)) s is
   runInstruction m s (s'@(Snd c):is) = runInstruction (M.insert '_' (lookup' m c) m) s is
   runInstruction m s (s'@(Rcv c):is) = if lookup' m c == 0 then runInstruction m s is else lookup' m '_'
-  runInstruction m s (s'@(JgzChar c c'):is) = if lookup' m c > 0 then runJgz m s (lookup' m c') s' is else runInstruction m s is
-  runInstruction m s (s'@(Jgz c i):is) = if lookup' m c > 0 then runJgz m s i s' is else runInstruction m s is
+  runInstruction m s (s'@(Jgz c i):is) = if lookup' m c > 0 then runJgz m s (lookup'' m i) s' is else runInstruction m s is
 
   runJgz :: M.Map Char Int -> [Instruction] -> Int -> Instruction -> [Instruction] -> Int
   runJgz m s v i is = let idx = findIndex' i s
@@ -55,13 +47,19 @@ module Advent.Day18 (day18a, day18b) where
     (Just i) -> i
     Nothing -> 0
 
+  lookup'' :: M.Map Char Int -> Value -> Int
+  lookup'' m (Number i) = i
+  lookup'' m (Register c) = case M.lookup c m of
+                          (Just i) -> i
+                          Nothing -> 0
+
   findIndex' :: Instruction -> [Instruction] -> Int
   findIndex' i is = case findIndex (==i) is of
     Nothing -> error "not found"
     (Just idx) -> idx
 
   parseInput :: String -> Either ParseError Instruction
-  parseInput = parse (choice [try parseSet, try parseSetChar, try parseAdd, try parseAddChar, try parseMul, try parseMulChar, try parseMod, try parseModChar, try parseJgz, try parseJgzChar, try parseSnd, parseRcv]) ""
+  parseInput = parse (choice [try parseSet, try parseAdd, try parseMul, try parseMod, try parseJgz, try parseSnd, parseRcv]) ""
 
   parseSet :: Parser Instruction
   parseSet = do
@@ -69,17 +67,8 @@ module Advent.Day18 (day18a, day18b) where
     _ <- space
     id <- letter
     _ <- space
-    val <- many1 $ choice [try $ char '-', digit]
-    return $ Set id (read val)
-
-  parseSetChar :: Parser Instruction
-  parseSetChar = do
-    _ <- string "set"
-    _ <- space
-    id <- letter
-    _ <- space
-    id' <- letter
-    return $ SetChar id id'
+    val <- many1 $ choice [try $ char '-', try digit, letter]
+    return $ Set id (if any isDigit val then Number $ read val else Register $ head val)
 
   parseAdd :: Parser Instruction
   parseAdd = do
@@ -87,17 +76,8 @@ module Advent.Day18 (day18a, day18b) where
     _ <- space
     id <- letter
     _ <- space
-    val <- many1 $ choice [try $ char '-', digit]
-    return $ Add id (read val)
-
-  parseAddChar :: Parser Instruction
-  parseAddChar = do
-    _ <- string "add"
-    _ <- space
-    id <- letter
-    _ <- space
-    id' <- letter
-    return $ AddChar id id'
+    val <- many1 $ choice [try $ char '-', try digit, letter]
+    return $ Add id (if any isDigit val then Number $ read val else Register $ head val)
 
   parseMul :: Parser Instruction
   parseMul = do
@@ -105,17 +85,8 @@ module Advent.Day18 (day18a, day18b) where
     _ <- space
     id <- letter
     _ <- space
-    val <- many1 $ choice [try $ char '-', digit]
-    return $ Mul id (read val)
-
-  parseMulChar :: Parser Instruction
-  parseMulChar = do
-    _ <- string "mul"
-    _ <- space
-    id <- letter
-    _ <- space
-    id' <- letter
-    return $ MulChar id id'
+    val <- many1 $ choice [try $ char '-', try digit, letter]
+    return $ Mul id (if any isDigit val then Number $ read val else Register $ head val)
 
   parseMod :: Parser Instruction
   parseMod = do
@@ -123,17 +94,8 @@ module Advent.Day18 (day18a, day18b) where
     _ <- space
     id <- letter
     _ <- space
-    val <- many1 $ choice [try $ char '-', digit]
-    return $ Mod id (read val)
-
-  parseModChar :: Parser Instruction
-  parseModChar = do
-    _ <- string "mod"
-    _ <- space
-    id <- letter
-    _ <- space
-    id' <- letter
-    return $ ModChar id id'
+    val <- many1 $ choice [try $ char '-', try digit, letter]
+    return $ Mod id (if any isDigit val then Number $ read val else Register $ head val)
 
   parseJgz :: Parser Instruction
   parseJgz = do
@@ -141,17 +103,8 @@ module Advent.Day18 (day18a, day18b) where
     _ <- space
     id <- letter
     _ <- space
-    val <- many1 $ choice [try $ char '-', digit]
-    return $ Jgz id (read val)
-
-  parseJgzChar :: Parser Instruction
-  parseJgzChar = do
-    _ <- string "jgz"
-    _ <- space
-    id <- letter
-    _ <- space
-    id' <- letter
-    return $ JgzChar id id'
+    val <- many1 $ choice [try $ char '-', try digit, letter]
+    return $ Jgz id (if any isDigit val then Number $ read val else Register $ head val)
 
   parseSnd :: Parser Instruction
   parseSnd = do
