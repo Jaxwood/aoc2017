70f185542b7d0df72765274bf8e17574a3a7131a
Jacob Lorenzen <lorenzen.jacob@gmail.com>
2018-01-07 21:06:11 +0100

    parse maze into tokens

--- src/Advent/Day19.hs
+++ src/Advent/Day19.hs
@@ -1,7 +1,22 @@
 module Advent.Day19 (day19a, day19b) where
 
+  data Token = Pipe Int | Cross Int | Dash Int | Letter Int Char deriving (Show,Eq)
+
   day19a :: String -> String
-  day19a = id
+  day19a s = let a = map (tokens . indexes) $ lines s
+             in ""
 
   day19b :: String -> Int
   day19b s = 0
+
+  indexes :: String -> [(Int,Char)]
+  indexes = zip [0..]
+
+  tokens :: [(Int,Char)] -> [Token]
+  tokens [] = []
+  tokens ((i,'|'):ts) = (Pipe i):tokens ts
+  tokens ((i,'+'):ts) = (Cross i):tokens ts
+  tokens ((i,'-'):ts) = (Dash i):tokens ts
+  tokens ((i,' '):ts) = tokens ts
+  tokens ((i,t):ts) = (Letter i t):tokens ts
+
