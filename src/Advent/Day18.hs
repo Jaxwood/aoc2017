83ad3eae36d5f9c317561319ad0fcef1f829a4d2
Jacob Lorenzen <lorenzen.jacob@gmail.com>
2018-01-07 19:16:51 +0100

    solution for day18b

--- src/Advent/Day18.hs
+++ src/Advent/Day18.hs
@@ -2,44 +2,67 @@ module Advent.Day18 (day18a, day18b) where
 
   import qualified Data.Map.Strict as M
   import Data.Char
+  import Data.Tuple (swap)
   import Data.List as L
   import Data.Either (rights)
   import Text.Parsec
+  import Text.Parsec.Error (messageString, errorMessages)
   import Text.Parsec.String
 
   type Queue = [Int]
   type Times = Int
-  data Program a b = Program [a] b (Maybe Queue) Times
+  data Program a b = Program [a] b Times deriving (Eq,Show)
   data Value = Number Int | Register Char deriving (Show,Eq) 
-  data Instruction = Set Char Value | Add Char Value | Mul Char Value | Mod Char Value | Snd Char | Rcv Char | Jgz Char Value deriving (Show,Eq)
+  data Instruction = Set Char Value | Add Char Value | Mul Char Value | Mod Char Value | Snd Char | Rcv Char | Jgz Value Value deriving (Show,Eq)
 
   instance Functor (Program a) where
-    fmap fn (Program (x:is) m q t) = Program is (fn m) q t
+    fmap fn (Program (x:is) m t) = Program is (fn m) t
 
   day18a :: String -> Int
   day18a s = let is = rights $ map parseInput $ lines s
-                 (Program _ m _ _) = runInstruction (Program is M.empty Nothing 0) is
+                 (Program _ m _) = runInstruction (Program is M.empty 0) is
              in lookup' m '_'
 
   day18b :: String -> Int
-  day18b s = let is = rights $ map parseInput $ lines s
-                 p = Program is (M.singleton 'p' 0) Nothing 0
-                 p' = Program is (M.singleton 'p' 1) Nothing 0
-             in runProgram p p' is
+  day18b s = let is = map (right . parseInput) $ lines s
+                 p = Program is (M.singleton 'p' 0) 0
+                 p' = Program is (M.singleton 'p' 1) 0
+             in runProgram p p' (Nothing,Nothing) is
 
-  runProgram :: Program Instruction (M.Map Char Int) -> Program Instruction (M.Map Char Int) -> [Instruction] -> Int
-  runProgram (Program _ _ (Just []) _) (Program _ _ (Just []) t) _ = 1
-  runProgram p p' is = 0
+  right :: Either ParseError Instruction -> Instruction
+  right (Right i) = i
+  right (Left e) = error (foldl1 (++) $ map messageString $ errorMessages e)
+
+  runProgram :: Program Instruction (M.Map Char Int) -> Program Instruction (M.Map Char Int) -> (Maybe Queue, Maybe Queue) -> [Instruction] -> Int
+  runProgram p (Program _ _ t) q@(Just [], Just []) _ = t
+  runProgram (Program [] _ _) (Program [] _ t) _ _ = t
+  runProgram p p' q is = let (a,q') = executeProgram p q is
+                             (b,q'') = executeProgram p' (swap q') is
+                         in runProgram a b (swap q'') is
+
+  executeProgram :: Program Instruction (M.Map Char Int) -> (Maybe Queue, Maybe Queue) -> [Instruction] -> (Program Instruction (M.Map Char Int), (Maybe Queue, Maybe Queue))
+  executeProgram p@(Program [] _ _) q  _ = (p, q)
+  executeProgram p@(Program ((Set c i):is) _ _ ) q is' = executeProgram (fmap (\m -> M.insert c (lookup'' m i) m) p) q is'
+  executeProgram p@(Program ((Add c i):is) _ _ ) q is'  = executeProgram (fmap (\m -> update' m (+) c (lookup'' m i)) p) q is'
+  executeProgram p@(Program ((Mul c i):is) _ _ ) q is' = executeProgram (fmap (\m -> update' m (*) c (lookup'' m i)) p) q is'
+  executeProgram p@(Program ((Mod c i):is) _ _ ) q is' = executeProgram (fmap (\m -> update' m mod c (lookup'' m i)) p) q is'
+  executeProgram p@(Program (s@(Jgz (Register c) i):is) m t) q is' = if lookup' m c > 0 then executeProgram (Program (runJgz is' (lookup'' m i) s) m t) q is' else executeProgram (fmap id p) q is'
+  executeProgram p@(Program (s@(Jgz (Number n) i):is) m t) q is' = if n > 0 then executeProgram (Program (runJgz is' (lookup'' m i) s) m t) q is' else executeProgram (fmap id p) q is'
+  executeProgram p@(Program ((Snd c):is) m t) (q,q') is' = executeProgram (Program is m (succ t)) (q,(enqueue q' (lookup' m c))) is'
+  executeProgram p@(Program ((Rcv c):is) m t) (q,q') is' = case dequeue q of
+      Nothing -> (p,(q,q'))
+      (Just (i, q'')) -> executeProgram (Program is (M.insert c i m) t) (Just q'',q') is'
 
   runInstruction :: Program Instruction (M.Map Char Int) -> [Instruction] -> Program Instruction (M.Map Char Int)
-  runInstruction p@(Program [] _ _ _) _ = p
-  runInstruction p@(Program ((Set c i):is) _ _ _) is' = runInstruction (fmap (\m -> M.insert c (lookup'' m i) m) p) is'
-  runInstruction p@(Program ((Add c i):is) _ _ _) is'  = runInstruction (fmap (\m -> update' m (+) c (lookup'' m i)) p) is'
-  runInstruction p@(Program ((Mul c i):is) _ _ _) is' = runInstruction (fmap (\m -> update' m (*) c (lookup'' m i)) p) is'
-  runInstruction p@(Program ((Mod c i):is) _ _ _) is' = runInstruction (fmap (\m -> update' m mod c (lookup'' m i)) p) is'
-  runInstruction p@(Program ((Snd c):is) _ _ _) is' = runInstruction (fmap (\m -> M.insert '_' (lookup' m c) m) p) is'
-  runInstruction p@(Program ((Rcv c):is) m _ _) is' = if lookup' m c == 0 then runInstruction (fmap id p) is' else p
-  runInstruction p@(Program (s@(Jgz c i):is) m q t) is' = if lookup' m c > 0 then runInstruction (Program (runJgz is' (lookup'' m i) s) m q t) is' else runInstruction (fmap id p) is'
+  runInstruction p@(Program [] _ _ ) _ = p
+  runInstruction p@(Program ((Set c i):is) _ _ ) is' = runInstruction (fmap (\m -> M.insert c (lookup'' m i) m) p) is'
+  runInstruction p@(Program ((Add c i):is) _ _ ) is'  = runInstruction (fmap (\m -> update' m (+) c (lookup'' m i)) p) is'
+  runInstruction p@(Program ((Mul c i):is) _ _ ) is' = runInstruction (fmap (\m -> update' m (*) c (lookup'' m i)) p) is'
+  runInstruction p@(Program ((Mod c i):is) _ _ ) is' = runInstruction (fmap (\m -> update' m mod c (lookup'' m i)) p) is'
+  runInstruction p@(Program ((Snd c):is) _ _ ) is' = runInstruction (fmap (\m -> M.insert '_' (lookup' m c) m) p) is'
+  runInstruction p@(Program ((Rcv c):is) m _) is' = if lookup' m c == 0 then runInstruction (fmap id p) is' else p
+  runInstruction p@(Program (s@(Jgz (Register c) i):is) m t) is' = if lookup' m c > 0 then runInstruction (Program (runJgz is' (lookup'' m i) s) m t) is' else runInstruction (fmap id p) is'
+  runInstruction p@(Program (s@(Jgz (Number n) i):is) m t) is' = if n > 0 then runInstruction (Program (runJgz is' (lookup'' m i) s) m t) is' else runInstruction (fmap id p) is'
 
   runJgz :: [Instruction] -> Int -> Instruction -> [Instruction]
   runJgz is v i = let idx = findIndex' i is
@@ -67,18 +90,18 @@ module Advent.Day18 (day18a, day18b) where
   enqueue Nothing i = Just [i]
   enqueue (Just is) i = Just (i:is)
 
-  dequeue :: Maybe Queue -> Maybe (Int, Maybe Queue)
+  dequeue :: Maybe Queue -> Maybe (Int, Queue)
   dequeue Nothing = Nothing
   dequeue (Just is) = case null is of
     True -> Nothing
-    False -> Just (last is, Just $ init is)
+    False -> Just (last is, init is)
 
   parseInput :: String -> Either ParseError Instruction
-  parseInput = parse (choice [try parseInstruction, parseInstruction']) ""
+  parseInput = parse (choice [try parseInstruction, try parseJgz, parseInstruction']) ""
 
   parseInstruction :: Parser Instruction
   parseInstruction = do
-    instruction <- choice [try $ string "mul", try $ string "mod", try $ string "jgz", try $ string "add", string "set"]
+    instruction <- choice [try $ string "mul", try $ string "mod", try $ try $ string "add", string "set"]
     _ <- space
     id <- letter
     _ <- space
@@ -88,7 +111,15 @@ module Advent.Day18 (day18a, day18b) where
                "add" -> Add id (if any isDigit val then Number $ read val else Register $ head val)
                "set" -> Set id (if any isDigit val then Number $ read val else Register $ head val)
                "mod" -> Mod id (if any isDigit val then Number $ read val else Register $ head val)
-               "jgz" -> Jgz id (if any isDigit val then Number $ read val else Register $ head val)
+
+  parseJgz :: Parser Instruction
+  parseJgz = do
+    _ <- string "jgz"
+    _ <- space
+    id <- many1 $ choice [try $ char '-', try digit, letter]
+    _ <- space
+    val <- many1 $ choice [try $ char '-', try digit, letter]
+    return $ Jgz (if any isDigit id then Number $ read id else Register $ head id) (if any isDigit val then Number $ read val else Register $ head val)
 
   parseInstruction' :: Parser Instruction
   parseInstruction' = do
--- test/Advent/Day18Spec.hs
+++ test/Advent/Day18Spec.hs
@@ -2,6 +2,7 @@ module Advent.Day18Spec (spec) where
 
   import Test.Hspec
   import System.Directory
+  import Data.Map
   import Advent.Day18 (day18a, day18b)
 
   main :: IO ()
@@ -16,7 +17,7 @@ module Advent.Day18Spec (spec) where
         csv <- readFile (dir ++ "/test/day18.csv")
         day18a csv `shouldBe` 4
     describe "day18b" $ do
-      it "TBD" $ do
+      it "should find times snd is called from program 1" $ do
         dir <- getCurrentDirectory
-        csv <- readFile (dir ++ "/test/day18.csv")
-        day18b csv `shouldBe` 0
+        csv <- readFile (dir ++ "/test/day18a.csv")
+        day18b csv `shouldBe` 3
