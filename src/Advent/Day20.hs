c0e143422ef66b1f0c2575c2173ee38b86ffa092
Jacob Lorenzen <lorenzen.jacob@gmail.com>
2018-01-08 20:33:58 +0100

    setup for day20

---
+++ src/Advent/Day20.hs
@@ -0,0 +1,7 @@
+module Advent.Day20 (day20a, day20b) where
+
+  day20a :: String -> Int
+  day20a s = 0
+
+  day20b :: String -> Int
+  day20b s = 0
---
+++ test/Advent/Day20Spec.hs
@@ -0,0 +1,17 @@
+module Advent.Day20Spec (spec) where
+
+  import Test.Hspec
+  import System.Directory
+  import Advent.Day20 (day20a, day20b)
+
+  main :: IO ()
+  main = do
+    hspec spec
+
+  spec :: Spec
+  spec = do
+    describe "day20a" $ do
+      it "should find particle closest to <0,0,0>" $ do
+        dir <- getCurrentDirectory
+        csv <- readFile (dir ++ "/test/day20.csv")
+        day20a csv `shouldBe` 0
