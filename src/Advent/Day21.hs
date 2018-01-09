e7cd45ac659366ef43a7ad7ee8df2bee509d3589
Jacob Lorenzen <lorenzen.jacob@gmail.com>
2018-01-09 23:27:46 +0100

    setup for day 21

---
+++ src/Advent/Day21.hs
@@ -0,0 +1,7 @@
+module Advent.Day21 (day21a, day21b) where
+
+  day21a :: String -> Int
+  day21a s = 0
+
+  day21b :: String -> Int
+  day21b s = 0
---
+++ test/Advent/Day21Spec.hs
@@ -0,0 +1,17 @@
+module Advent.Day21Spec (spec) where
+
+  import Test.Hspec
+  import System.Directory
+  import Advent.Day21 (day21a, day21b)
+
+  main :: IO ()
+  main = do
+    hspec spec
+
+  spec :: Spec
+  spec = do
+    describe "day21a" $ do
+      it "find pixel on after 2 iterations" $ do
+        dir <- getCurrentDirectory
+        csv <- readFile (dir ++ "/test/day21.csv")
+        day21a csv `shouldBe` 12
