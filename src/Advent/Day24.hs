328715599506b319ad34715e99de0f9be0c0c71f
Jacob Lorenzen <lorenzen.jacob@gmail.com>
2018-01-21 07:48:58 +0100

    Feature
    - add parsing for input

--- src/Advent/Day24.hs
+++ src/Advent/Day24.hs
@@ -1,7 +1,30 @@
 module Advent.Day24 (day24a, day24b) where
 
+  import Text.Parsec
+  import Text.Parsec.String
+
+  data Bridge = Bridge Int Int
+
   day24a :: String -> Int
   day24a s = 0
 
   day24b :: String -> Int
   day24b s = 0
+
+  -- utility
+
+  right :: Either ParseError Bridge -> Bridge
+  right (Left e) = error $ show e
+  right (Right r) = r
+
+  -- parse 
+
+  parseInput :: String -> Either ParseError Bridge
+  parseInput = parse parseBridge ""
+
+  parseBridge :: Parser Bridge
+  parseBridge = do
+    str <- many1 digit
+    _ <- char '/'
+    str' <- many1 digit
+    return $ Bridge (read str) (read str')
