01cab78f0e6d84e00422fb77183d1398aaed28e9
Jacob Lorenzen <lorenzen.jacob@gmail.com>
2018-01-21 07:34:37 +0100

    Feature
    - add boilerplate for day24

---
+++ src/Advent/Day24.hs
@@ -0,0 +1,7 @@
+module Advent.Day24 (day24a, day24b) where
+
+  day24a :: String -> Int
+  day24a s = 0
+
+  day24b :: String -> Int
+  day24b s = 0
---
+++ test/Advent/Day24Spec.hs
@@ -0,0 +1,17 @@
+module Advent.Day24Spec (spec) where
+
+  import Test.Hspec
+  import System.Directory
+  import Advent.Day24 (day24a, day24b)
+
+  main :: IO ()
+  main = do
+    hspec spec
+  
+  spec :: Spec
+  spec = do
+    describe "day24a" $ do
+     it "should find the strongest bridge" $ do
+        dir <- getCurrentDirectory
+        csv <- readFile (dir ++ "/test/day24.csv")
+        day24a csv `shouldBe` 0
